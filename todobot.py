fbe4e2bb818f47bfc3aa0ece85684156ef26e441
ymshenyu <ymshenyu@gmail.com>
2020-05-08 16:18:41 +0800

    Fix idiot bot

--- todobot.py
+++ todobot.py
@@ -53,10 +53,7 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("todo", todo))
 
     # log all errors
     dp.add_error_handler(error)
