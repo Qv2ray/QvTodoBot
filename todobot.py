02b3879f470d44a3a8031887980c6e3dd25dca51
ymshenyu <ymshenyu@gmail.com>
2020-05-09 16:45:56 +0800

    debug

--- todobot.py
+++ todobot.py
@@ -74,14 +74,14 @@ def todo(update, context):
 
 def remove(update, context):
     user_data = context.user_data
-    try:
+#    try:
         index = int(update.message.text.split(' ')[1]) - 1
         todo_list = user_data['todo']
         todo_list.pop(index)
         message = format_data(todo_list)
         update.message.reply_text(message)
-    except Exception:
-        update.message.reply_text('An error occurred')
+#    except Exception:
+#        update.message.reply_text('An error occurred')
 
 
 def toggle(update, context):
