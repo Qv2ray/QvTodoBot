eb2dc226c7694d1520004f5296fa0f80ed3fda21
database64128 <free122448@hotmail.com>
2020-11-26 00:47:18 +0800

    🎳 Add `/bowl` and refine number parsing

--- modules/dart.py
+++ modules/dart.py
@@ -1,4 +1,3 @@
-import modules.expression as expression
 from telegram.bot import Bot
 from telegram.ext import CommandHandler, CallbackContext
 from telegram import Update, Message
@@ -14,15 +13,18 @@ class Darter:
                 CommandHandler("dice", self.dice),
                 CommandHandler("basketball", self.basketball),
                 CommandHandler("soccer", self.soccer),
-                CommandHandler("roll", self.roll)]
+                CommandHandler("roll", self.roll),
+                CommandHandler("bowl", self.bowl)]
 
     def dart(self, update: Update, context: CallbackContext):
         assert isinstance(update.message, Message)
         assert isinstance(update.message.text, str)
-        text = ''.join(map(str, update.message.text.split(' ')[1:]))
-        nsp = expression.NumericStringParser()
+        parsed_message = update.message.text.split(' ', 1)
         try:
-            times = int(nsp.eval(text))
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
         except Exception:
             times = 1
         for i in range(times):
@@ -31,10 +33,12 @@ class Darter:
     def dice(self, update: Update, context: CallbackContext):
         assert isinstance(update.message, Message)
         assert isinstance(update.message.text, str)
-        text = ''.join(map(str, update.message.text.split(' ')[1:]))
-        nsp = expression.NumericStringParser()
+        parsed_message = update.message.text.split(' ', 1)
         try:
-            times = int(nsp.eval(text))
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
         except Exception:
             times = 1
         for i in range(times):
@@ -43,10 +47,12 @@ class Darter:
     def basketball(self, update: Update, context: CallbackContext):
         assert isinstance(update.message, Message)
         assert isinstance(update.message.text, str)
-        text = ''.join(map(str, update.message.text.split(' ')[1:]))
-        nsp = expression.NumericStringParser()
+        parsed_message = update.message.text.split(' ', 1)
         try:
-            times = int(nsp.eval(text))
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
         except Exception:
             times = 1
         for i in range(times):
@@ -55,10 +61,12 @@ class Darter:
     def soccer(self, update: Update, context: CallbackContext):
         assert isinstance(update.message, Message)
         assert isinstance(update.message.text, str)
-        text = ''.join(map(str, update.message.text.split(' ')[1:]))
-        nsp = expression.NumericStringParser()
+        parsed_message = update.message.text.split(' ', 1)
         try:
-            times = int(nsp.eval(text))
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
         except Exception:
             times = 1
         for i in range(times):
@@ -67,11 +75,27 @@ class Darter:
     def roll(self, update: Update, context: CallbackContext):
         assert isinstance(update.message, Message)
         assert isinstance(update.message.text, str)
-        text = ''.join(map(str, update.message.text.split(' ')[1:]))
-        nsp = expression.NumericStringParser()
+        parsed_message = update.message.text.split(' ', 1)
         try:
-            times = int(nsp.eval(text))
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
         except Exception:
             times = 1
         for i in range(times):
             self.bot.send_dice(chat_id=update.message.chat_id, emoji='🎰')
+
+    def bowl(self, update: Update, context: CallbackContext):
+        assert isinstance(update.message, Message)
+        assert isinstance(update.message.text, str)
+        parsed_message = update.message.text.split(' ', 1)
+        try:
+            if len(parsed_message) == 2:
+                times = int(parsed_message[1])
+            else:
+                times = 1
+        except Exception:
+            times = 1
+        for i in range(times):
+            self.bot.send_dice(chat_id=update.message.chat_id, emoji='🎳')
